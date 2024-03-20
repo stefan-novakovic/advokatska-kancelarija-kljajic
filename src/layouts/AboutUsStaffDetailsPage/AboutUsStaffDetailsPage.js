8019b416eabfb5df4f9433125accafd838ba7a94
Stefan Novakovic <snovakovic3@gmail.com>
2024-03-20 22:34:40 +0100

    Added to staff details page; todo: responsive for smaller screens

--- src/layouts/AboutUsStaffDetailsPage/AboutUsStaffDetailsPage.js
+++ src/layouts/AboutUsStaffDetailsPage/AboutUsStaffDetailsPage.js
@@ -1,9 +1,38 @@
 import { StyledAboutUsStaffDetailsPage } from "./AboutUsStaffDetailsPage.styled";
+import { useParams } from "react-router-dom";
+import { aboutUsStaffArray } from "../../data/data";
+import { HiOutlinePhone } from "react-icons/hi";
+import { GoMail } from "react-icons/go";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/opacity.css";
 
 const AboutUsStaffDetailsPage = () => {
+  const { id } = useParams();
+  const staffMember = aboutUsStaffArray.find((member) => id === member.id);
   return (
     <StyledAboutUsStaffDetailsPage>
-      <h2>AboutUsStaffDetailsPage</h2>
+      <h2>Osoblje</h2>
+
+      <section>
+        <div>
+          <LazyLoadImage src={staffMember.image} alt="" effect="opacity" />
+        </div>
+
+        <div>
+          <h3>{staffMember.fullName}</h3>
+          <h4>{staffMember.role.toUpperCase()}</h4>
+          <a href={`mailto:${staffMember.email}`}>
+            <GoMail /> {staffMember.email}
+          </a>
+          <a href={`tel:${staffMember.phoneLink}`}>
+            <HiOutlinePhone /> <span>{staffMember.phone}</span>
+          </a>
+        </div>
+      </section>
+      <section>
+        <h3>Biografija</h3>
+        <p>{staffMember.bio}</p>
+      </section>
     </StyledAboutUsStaffDetailsPage>
   );
 };
--- src/layouts/AboutUsStaffDetailsPage/AboutUsStaffDetailsPage.styled.js
+++ src/layouts/AboutUsStaffDetailsPage/AboutUsStaffDetailsPage.styled.js
@@ -25,12 +25,110 @@ export const StyledAboutUsStaffDetailsPage = styled.div`
     -ms-user-select: none;
   }
 
+  & > section:first-of-type {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 7rem;
+    width: 100%;
+    max-width: min(76vw, 1500px);
+    margin-inline: auto;
+    margin-bottom: 6rem;
+  }
+
+  & > section:first-of-type > div:first-child {
+    display: flex;
+    width: 100%;
+    max-width: 435px;
+    aspect-ratio: 435/555.22;
+    height: auto;
+    border-radius: 50%;
+    background-color: rgba(242, 246, 249, 0.06);
+  }
+
+  & > section:first-of-type > div:first-child img {
+    border-radius: 50%;
+  }
+
+  section:first-of-type > div:last-child > h3 {
+    font-size: 4.25rem;
+    letter-spacing: 2px;
+    margin-bottom: 1rem;
+  }
+
+  section:first-of-type > div:last-child > h4 {
+    font-size: 2rem;
+    font-weight: 500;
+    letter-spacing: 0.5px;
+    margin-bottom: 2.5rem;
+  }
+
+  section:first-of-type > div:last-child > a:any-link {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    width: fit-content;
+    font-size: 1.6rem;
+    font-weight: 400;
+    letter-spacing: 0.5px;
+    color: ${({ theme }) => theme.colors.veryLightBlue};
+    text-decoration: none;
+    margin-bottom: 1.35rem;
+  }
+
+  section:first-of-type > div:last-child > a:any-link:last-child {
+    margin-bottom: 0;
+  }
+
+  section:first-of-type > div:last-child > a:any-link span {
+    white-space: nowrap;
+  }
+
+  section:first-of-type > div:last-child > a:any-link svg {
+    width: 100%;
+    height: auto;
+    min-width: 28px;
+    max-width: 28px;
+    margin-right: 0.75rem;
+  }
+
+  section:last-of-type {
+    width: 100%;
+    max-width: min(76vw, 1500px);
+    margin-inline: auto;
+  }
+
+  section:last-of-type > h3 {
+    font-size: 2rem;
+    font-weight: 500;
+    letter-spacing: 0.75px;
+    margin-bottom: 2.5rem;
+  }
+
+  section:last-of-type > p {
+    white-space: pre-wrap;
+    font-size: 1.3rem;
+    font-weight: 400;
+    line-height: 1.4;
+    letter-spacing: 0.5px;
+  }
+
   @media screen and (max-width: 1023px) {
     min-height: calc(100vh - 82px + 1px);
+
+    & > section:first-of-type {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap: 4rem;
+    }
   }
 
   @media screen and (max-width: 1536px) {
-    h2 {
+    h2,
+    section:first-of-type,
+    section:last-of-type {
       max-width: 88vw;
     }
   }
