205c8112ff64a9ca38a064b0f5c269bb134d8c85
Stefan Novakovic <snovakovic3@gmail.com>
2024-04-16 23:14:35 +0200

    Added email to onSubmit function

--- src/components/ContactUsPageFormContainer/onSubmit.js
+++ src/components/ContactUsPageFormContainer/onSubmit.js
@@ -6,14 +6,15 @@ export const onSubmit = async (data) => {
       data.firstName.slice(0, 1).toUpperCase() + data.firstName.slice(1);
     const lastName =
       data.lastName.slice(0, 1).toUpperCase() + data.lastName.slice(1);
+    const email = data.email.toLowerCase();
     const message =
       data.message.slice(0, 1).toUpperCase() + data.message.slice(1);
 
-    console.log(JSON.stringify({ firstName, lastName, message }));
+    console.log(JSON.stringify({ firstName, lastName, email, message }));
 
     // const response = await axios.post(
     //   "",
-    //   JSON.stringify({ firstName, lastName, message })
+    //   JSON.stringify({ firstName, lastName, email, message })
     // );
   } catch (err) {
     // if (err.response) {
