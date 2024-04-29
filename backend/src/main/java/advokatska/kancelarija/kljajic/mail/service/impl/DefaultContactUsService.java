fd3e6eff6ced1dd4ab8cb33ccbd2b95780c6b99a
King Maus <the.k1ng.maus.official@gmail.com>
2024-04-29 10:17:03 +0200

    fix

--- backend/src/main/java/advokatska/kancelarija/kljajic/mail/service/impl/DefaultContactUsService.java
+++ backend/src/main/java/advokatska/kancelarija/kljajic/mail/service/impl/DefaultContactUsService.java
@@ -8,14 +8,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.apache.commons.text.StringSubstitutor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -24,6 +27,7 @@ public class DefaultContactUsService implements ContactUsService {
     @Value("${mail.recipients}")
     private final Set<String> recipientsEmail;
     private final EmailService emailService;
+    private final ResourceLoader resourceLoader;
 
     @Override
     public void contactUs(@NonNull SendMailRequestDto request) {
@@ -48,7 +52,10 @@ public class DefaultContactUsService implements ContactUsService {
 
     @SneakyThrows
     private String loadContactUsTemplate() {
-        Path path = new ClassPathResource("templates/contactUsTemplate.html").getFile().toPath();
-        return Files.readString(path, StandardCharsets.UTF_8);
+        Resource resource = resourceLoader.getResource("classpath:templates/contactUsTemplate.html");
+        try (InputStream inputStream = resource.getInputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
     }
 }
